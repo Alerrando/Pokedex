d6f5301a6bc54713f793b4965c681c9929e20f15
Alerrando Breno <alerrando2@gmail.com>
2022-10-13 22:52:13 -0300

    calling api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { Navbar } from './components/Navbar'
 import './App.css'
+import { Searchbar } from './components/Searchbar'
+import { searchPokemon } from './api'
 
 export function App() {
+
+  async function onSearchHandler(pokemon:string){
+    const result = await searchPokemon(pokemon);
+
+    console.log(result)
+  }
 
   return (
     <div>
       <Navbar />
+      <Searchbar onSearch={onSearchHandler} />
     </div>
   )
 }
--- src/components/Searchbar/index.tsx
+++ src/components/Searchbar/index.tsx
@@ -1,9 +1,26 @@
-export function Searchbar(){
+import { ChangeEvent } from "react"
+import { useState } from "react";
+
+type SearchBarProps = {
+    onSearch: (pokemon:string) => void;
+}
+
+export function Searchbar(props:SearchBarProps){
+    const { onSearch } = props;
+    const [search, setSearch] = useState("charizard")
+    
     return(
-        <div>
-            <div>
-                <input type="text" placeholder="buscar pokemon" />
+        <div className="searchbar-container">
+            <div className="searchbar">
+                <input type="text" placeholder="buscar pokemon" onChange={e => setSearch((e.target as HTMLInputElement).value)} />
+            </div>
+            <div className="searchbar-btn">
+                <button onClick={onButtonClickHandler}>Buscar</button>
             </div>
         </div>
     )
+
+    function onButtonClickHandler(){
+       const pokemon = onSearch(search);
+    }
 }
