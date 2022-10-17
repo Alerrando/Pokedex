044bb57b539ab876a5d9d838914e5b5f9935e2a8
Alerrando Breno <alerrando2@gmail.com>
2022-10-16 23:56:45 -0300

    add emote hearth

--- src/components/Pokemon/index.tsx
+++ src/components/Pokemon/index.tsx
@@ -1,17 +1,45 @@
 type PokemonProps = {
-    pokemon: any;
-}
+  pokemon: any;
+};
 
-export function Pokemon(props:PokemonProps){
-    const { pokemon } = props;
+export function Pokemon(props: PokemonProps) {
+  const { pokemon } = props;
+  const hearth = "❤️"
 
-    return(
-        <div className="pokemon-card">
-            <div className="pokmeon-image-contai">
-                <img src={pokemon.sprites.front_default} alt={pokemon.name} className="pokemon-image" />
-            </div>
-            
-            {pokemon.name}
+  return (
+    <div className="pokemon-card">
+      <div className="pokmeon-image-contai">
+        <img
+          src={pokemon.sprites.front_default}
+          alt={pokemon.name}
+          className="pokemon-image"
+        />
+      </div>
+
+      <div className="card-body">
+        <div className="card-top">
+          <h3>{pokemon.name}</h3>
+          <div>#{pokemon.id}</div>
         </div>
-    )
+      </div>
+
+      <div className="card-bottom">
+        <div className="pokemon-type">
+          {pokemon?.types.map((type: any, index: number) => (
+                <div className="pokemon-type-text" key={index}>
+                  {type.type.name}
+                </div>
+          ))}
+        </div>
+
+        <button className="pokemon-hearth-btn" onClick={() => onHearthClick()}>
+            {hearth}
+        </button>
+      </div>
+    </div>
+  );
+
+  function onHearthClick(){
+    console.log("funcionou")
+  }
 }
