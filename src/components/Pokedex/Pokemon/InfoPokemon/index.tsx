486f0d9da929acaefeccfa4ed45c9749c95196c0
Alerrando Breno <alerrando2@gmail.com>
2022-10-26 15:41:19 -0300

    style infos-pokemon

--- src/components/Pokedex/Pokemon/InfoPokemon/index.tsx
+++ src/components/Pokedex/Pokemon/InfoPokemon/index.tsx
@@ -1,50 +1,63 @@
-import { useEffect, useState } from 'react';
-import { ArrowLeft, Star } from 'phosphor-react'
-import './style.css'
+import { useEffect, useState } from "react";
+import { ArrowLeft, Star } from "phosphor-react";
+import "./style.css";
 
 type InfoPokemonProps = {
-    setModal: (modal:boolean) => void;
-    clickPokemon: string,
-    pokemon:any
-}
+  setModal: (modal: boolean) => void;
+  clickPokemon: string;
+  pokemon: any;
+};
 
-export function InfoPokemon(props:InfoPokemonProps){
-    const { setModal, clickPokemon, pokemon } = props;
-    const [ infos, setInfos ] = useState<any>([]);
+export function InfoPokemon(props: InfoPokemonProps) {
+  const { setModal, clickPokemon, pokemon } = props;
+  const [infos, setInfos] = useState<any>([]);
 
-    useEffect(() => {
-        SearchPokemonClick();
-    }, [])
-    
-    
-    return(
-        <div className="container-info">
-            <div className="info-container" style={{
-                background: `url(Habitat/${pokemon.types[0].type.name}.jpeg)`,
-                backgroundSize: "cover"
-            }}>
-                <header className="info-header">
-                    <ArrowLeft size={32} onClick={() => setModal(false)} />
-                    <h2 className='h2'>{infos.name}</h2>
-                    <Star size={32} className="icon-star" />
-                </header>
+  useEffect(() => {
+    SearchPokemonClick();
+  }, []);
 
-                <section>
-                    <figure>
-                        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-                    </figure>
+  console.log(pokemon);
 
-                    <div className="container-infos-pokemon">
-                        
-                    </div>
-                </section>
+  return (
+    <div className="container-info">
+      <div
+        className="info-container"
+        style={{
+          background: `url(Habitat/${pokemon.types[0].type.name}.jpeg)`,
+          backgroundSize: "cover",
+        }}
+      >
+        <header className="info-header">
+          <ArrowLeft size={32} onClick={() => setModal(false)} />
+          <h2 className="h2">{infos.name}</h2>
+          <Star size={32} className="icon-star" />
+        </header>
+
+        <section>
+          <figure>
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+          </figure>
+
+          <div className="container-infos-pokemon">
+            <div className="infos-pokemon">
+              {pokemon.stats.map((stat:any) => (
+                <div className={`info-status ${stat.stat.name}`}>
+                  <p>{stat.stat.name}</p>
+                  <div className="status"></div>
+                </div>
+              ))}
             </div>
-        </div>
-    )
-    
-    async function SearchPokemonClick(){
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${clickPokemon}`);
-        const data = await res.json();
-        setInfos(data);
-    }
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+
+  async function SearchPokemonClick() {
+    const res = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${clickPokemon}`
+    );
+    const data = await res.json();
+    setInfos(data);
+  }
 }
